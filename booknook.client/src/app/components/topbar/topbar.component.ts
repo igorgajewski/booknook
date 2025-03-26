71d7a4051cc8ee178d3b889687fcbcc5af8ac0c9
igor <igorgajewski94@gmail.com>
2025-03-26 23:59:40 +0100

    add topbar

---
+++ booknook.client/src/app/components/topbar/topbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-topbar',
+  imports: [],
+  templateUrl: './topbar.component.html',
+  styleUrl: './topbar.component.scss'
+})
+export class TopbarComponent {
+@Output() toggleSidebar = new EventEmitter<void>();
+
+onToggleSidebar(): void {
+  this.toggleSidebar.emit();
+}
+}
