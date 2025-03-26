ad964e90853b07a0a0892c392a0272445cab1d65
igor <igorgajewski94@gmail.com>
2025-03-27 00:00:44 +0100

    add sidebar, topbar, mainpage, profile and settings pages drafts

--- booknook.client/src/app/app.component.ts
+++ booknook.client/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopbarComponent } from "./components/topbar/topbar.component";
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopbarComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'booknook.client';
+  sidebarCollapsed = false;
+  onToggleSidebar(): void {
+    this.sidebarCollapsed = !this.sidebarCollapsed;
+  }
 }
--- booknook.client/src/app/app.routes.ts
+++ booknook.client/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MainpageComponent } from './components/mainpage/mainpage.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/mainpage', pathMatch: 'full' },
+    { path: 'mainpage', component: MainpageComponent },
+    { path: 'profile', component: ProfileComponent },
+    { path: 'settings', component: SettingsComponent },
+];
