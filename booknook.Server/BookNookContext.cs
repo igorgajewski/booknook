3fa78113c5d392b4c1fc96b884093a3e10870561
igor <igorgajewski94@gmail.com>
2025-04-10 21:14:00 +0200

    some work on sidebar

---
+++ booknook.Server/BookNookContext.cs
@@ -0,0 +1,13 @@
+﻿using booknook.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace booknook.Server;
+
+public class BookNookContext : DbContext
+{
+    public BookNookContext(DbContextOptions<BookNookContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<User> Users { get; set; }
+}
---
+++ booknook.Server/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace booknook.Server.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string EmailAddress { get; set; }
+}
